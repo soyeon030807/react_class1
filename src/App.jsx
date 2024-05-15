d59b5015048c1e6aebf2695397710b0b6f87207a
JangHwanPark <wkd5878@gmail.com>
2024-05-15 23:48:32 +0900

    feat: 댓글 작성 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import {Outlet} from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 export default function App() {
-    return (
-        <>
-            <Header title="💜소연이의 블로그💜"/>
-            <Outlet/>
-            <Footer/>
-        </>
-    )
+    return <Outlet/>
 }
--- src/components/page/PostDetail.jsx
+++ src/components/page/PostDetail.jsx
@@ -1,37 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useParams} from "react-router-dom";
 import Comments from "../Comments";
 import posts from "../../data/posts.json";
+import PageLayout from "./PageLayout";
 
 export default function PostDetail() {
     const params = useParams();
     const post = posts.find(item => item.id === +params.id);
     const {title, date, content} = post
-    console.log(post)
+
+    const [comment, setComment] = useState([]);
+    console.log(`PostDetail`)
+    console.log(comment)
+    console.log(`PostDetail`)
 
     return (
-        <main>
-            <section>
-                <h2>{title}</h2>
-                <div>작성일: {date}</div>
-            </section>
-            <section>
-                <h3>{content.intro.title}</h3>
-                <article>
-                    {content.intro.text}
-                </article>
-            </section>
-            {content.travel.map(item => (
-                <section key={item.title}>
-                    <h3>{item.title}</h3>
+        <PageLayout>
+            <main className="main-details flex flex-col">
+                <section className="section-post">
+                    <h2>{title}</h2>
+                    <div>작성일: {date}</div>
+                </section>
+                <section className="section-post">
+                    <h3>{content.intro.title}</h3>
                     <article>
-                        {item.items}
+                        {content.intro.text}
                     </article>
                 </section>
-            ))}
-            <section>
-                <Comments/>
+                {content.travel.map(item => (
+                    <section key={item.title} className="section-post">
+                        <h3>{item.title}</h3>
+                        <article>
+                            {item.items}
+                        </article>
+                    </section>
+                ))}
+            </main>
+            <section className="section-commnet">
+                <Comments comment={comment} setComment={setComment}/>
+                <ul>
+                    {comment.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
             </section>
-        </main>
+        </PageLayout>
     );
 }
